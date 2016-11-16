86934f008fb68a79b8b16faecedc600e0720862c
Danil Kozyatnikov <dan@aliendevs.com>
2016-11-15 16:15:06 -0800

    Create sms.php
    Initial text forwarding script.

---
+++ sms.php
@@ -0,0 +1,11 @@
+<?php
+
+header('Content-Type: text/html');
+echo '<Response>';
+    if( isset($_REQUEST['to']) && isset($_REQUEST['From']) && isset($_REQUEST['Body']) ){
+        $to = split(',', $_REQUEST['to'])
+        foreach( $to as $recipient ){
+            echo '<Message to="'.$recipient.'">'.htmlspecialchars(substr($_REQUEST['From'].': '.$_REQUEST['Body'], 0, 1600))'</Message>';
+        }
+    }
+echo '</Response>';
