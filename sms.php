aa1b4b10039a0f190fcfba9951fddf1cfd8b9864
Danil Kozyatnikov <dan@aliendevs.com>
2016-11-15 16:25:55 -0800

    Typo

--- sms.php
+++ sms.php
@@ -3,7 +3,7 @@
 header('Content-Type: text/html');
 echo '<Response>';
     if( isset($_REQUEST['to']) && isset($_REQUEST['From']) && isset($_REQUEST['Body']) ){
-        $to = split(',', $_REQUEST['to'])
+        $to = split(',', $_REQUEST['to']);
         foreach( $to as $recipient ){
             echo '<Message to="'.$recipient.'">'.htmlspecialchars(substr($_REQUEST['From'].': '.$_REQUEST['Body'], 0, 1600))'</Message>';
         }
