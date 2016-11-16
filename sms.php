d5d3a4001e45eae74c74120191f418f2dd6e3955
Danil Kozyatnikov <dan@aliendevs.com>
2016-11-15 16:26:48 -0800

    Another typo

--- sms.php
+++ sms.php
@@ -5,7 +5,7 @@ echo '<Response>';
     if( isset($_REQUEST['to']) && isset($_REQUEST['From']) && isset($_REQUEST['Body']) ){
         $to = split(',', $_REQUEST['to']);
         foreach( $to as $recipient ){
-            echo '<Message to="'.$recipient.'">'.htmlspecialchars(substr($_REQUEST['From'].': '.$_REQUEST['Body'], 0, 1600))'</Message>';
+            echo '<Message to="'.$recipient.'">'.htmlspecialchars(substr($_REQUEST['From'].': '.$_REQUEST['Body'], 0, 1600)).'</Message>';
         }
     }
 echo '</Response>';
